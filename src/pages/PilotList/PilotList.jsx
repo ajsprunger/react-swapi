8fc54de73059d2849c5c8b8c2592fb7123acc905
austinjs <ajsprunger@gmail.com>
2022-03-17 16:10:39 -0600

    bonus returns pilotnames but doesnt work

---
+++ src/pages/PilotList/PilotList.jsx
@@ -0,0 +1,12 @@
+
+async function getPilots(urls) {
+  const promises = urls.map(url => fetch(url).then(res => res.json()))
+  const pilotObjects = await Promise.all(promises)
+  const pilotNames =  pilotObjects.map(pilot => {
+    return pilot.name  
+  })
+  return pilotNames
+}
+
+export default getPilots
+
--- src/pages/ShipDetails/ShipDetails.jsx
+++ src/pages/ShipDetails/ShipDetails.jsx
@@ -1,9 +1,13 @@
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom'
+import getPilots from '../PilotList/PilotList';
 
 const ShipDetails = () => {
   let location = useLocation()
   let shipDetails = location.state.shipName
+  const pilotUrls = location.state.shipName.pilots
+
+  console.log('get', getPilots(pilotUrls))
 
   return(
     <>
@@ -12,6 +16,15 @@ const ShipDetails = () => {
         <div className='ship-details'>
           <p>NAME: {shipDetails.name}</p>
           <p>MODEL: {shipDetails.model}</p>
+          {pilotUrls.length ?
+          <div>
+            {getPilots(pilotUrls).map(pilotName => {
+              return <div>{pilotName}</div>
+            })}
+          </div>
+          :
+          <p>No Pilots</p>
+          } 
           <Link className="link" to='/'>RETURN</Link>
         </div>
         :
