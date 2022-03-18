4a0cbf27d379334d1a9c2e3eb03596e8588b1916
austinjs <ajsprunger@gmail.com>
2022-03-18 07:28:27 -0600

    finish bonus

--- src/pages/PilotList/PilotList.jsx
+++ src/pages/PilotList/PilotList.jsx
@@ -1,6 +1,6 @@
 
 async function getPilots(urls) {
-  const promises = urls.map(url => fetch(url).then(res => res.json()))
+  const promises = await urls.map(url => fetch(url).then(res => res.json()))
   const pilotObjects = await Promise.all(promises)
   const pilotNames =  pilotObjects.map(pilot => {
     return pilot.name  
--- src/pages/ShipDetails/ShipDetails.jsx
+++ src/pages/ShipDetails/ShipDetails.jsx
@@ -1,13 +1,21 @@
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import getPilots from '../PilotList/PilotList';
 
+
 const ShipDetails = () => {
+  const [names, setNames] = useState([])
   let location = useLocation()
   let shipDetails = location.state.shipName
   const pilotUrls = location.state.shipName.pilots
 
-  console.log('get', getPilots(pilotUrls))
+  useEffect(() => {
+    const retrieve = async() => {
+      const pilots = await getPilots(pilotUrls)
+      setNames(pilots)
+    }  
+    retrieve()
+  }, [pilotUrls])
 
   return(
     <>
@@ -18,7 +26,7 @@ const ShipDetails = () => {
           <p>MODEL: {shipDetails.model}</p>
           {pilotUrls.length ?
           <div>
-            {getPilots(pilotUrls).map(pilotName => {
+            {names.map(pilotName => {
               return <div>{pilotName}</div>
             })}
           </div>
